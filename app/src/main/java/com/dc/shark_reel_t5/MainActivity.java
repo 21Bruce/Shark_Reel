1a985d557cc850361b62a2384131774dd8bd19f0
21Bruce <57542085+21bruce@users.noreply.github.com>
2020-08-13 14:14:18 -0400

    Fixed a bug regarding exports and permissions. Now exporting should not throw a security exception

--- app/src/main/java/com/dc/shark_reel_t5/MainActivity.java
+++ app/src/main/java/com/dc/shark_reel_t5/MainActivity.java
@@ -2,6 +2,8 @@ package com.dc.shark_reel_t5;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -19,6 +21,7 @@ import com.dc.shark_reel_t5.ui.main.SectionsPagerAdapter;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
 
 import static com.dc.shark_reel_t5.ui.main.Hooks.*;
 
@@ -87,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 //this is because it can store both commas and spaces, both critical to CSVs
                 StringBuilder data = new StringBuilder();
 
+
                 //add the column names from Hooks
                 for (int index = 0; index < getDataTypeLength(); index++) {
 
@@ -125,6 +129,9 @@ public class MainActivity extends AppCompatActivity {
                     fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
                     fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    fileIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                    fileIntent.setData(path);
                     startActivity(Intent.createChooser(fileIntent, "send mail"));
 
                 } catch (Exception e) {
